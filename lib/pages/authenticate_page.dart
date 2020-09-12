17c83cf4666311d7435a0e5a61dd8e102e06b897
adarsh189-pal <64680183+adarsh189-pal@users.noreply.github.com>
2020-09-12 17:27:52 +0530

    .

---
+++ lib/pages/authenticate_page.dart
@@ -0,0 +1,27 @@
+import 'package:chat/pages/register_page.dart';
+import 'package:chat/pages/signin_page.dart';
+import 'package:flutter/material.dart';
+
+class AuthenticatePage extends StatefulWidget {
+  @override
+  _AuthenticatePageState createState() => _AuthenticatePageState();
+}
+
+class _AuthenticatePageState extends State<AuthenticatePage> {
+  bool _showSignIn = true;
+
+  void _toggleView() {
+    setState(() {
+      _showSignIn = !_showSignIn;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (_showSignIn) {
+      return SignInPage(toggleView: _toggleView);
+    } else {
+      return RegisterPage(toggleView: _toggleView);
+    }
+  }
+}
